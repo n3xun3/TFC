4ed7ef7779f595cd9ec365e06746f3fde8b87c76
Jorge Ruiz Martínez <jorgetafd@gmail.com>
2024-06-05 19:31:16 +0200

    Añadimos sql para añadir asignaturas

--- insert_asignaturas.sql
+++ insert_asignaturas.sql
@@ -1,18 +1,4 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.4.8-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win64
--- HeidiSQL Versión:             10.2.0.5599
--- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- Volcando datos para la tabla db_microservicios_examenes.asignaturas: ~25 rows (aproximadamente)
-/*!40000 ALTER TABLE `asignaturas` DISABLE KEYS */;
 INSERT INTO `asignaturas` (`id`, `nombre`, `padre_id`) VALUES
 	(1, 'Matemáticas', NULL),
 	(2, 'Lenguaje', NULL),
@@ -39,8 +25,4 @@ INSERT INTO `asignaturas` (`id`, `nombre`, `padre_id`) VALUES
 	(23, 'Filosofía', 5),
 	(24, 'Música', 6),
 	(25, 'Artes', 7);
-/*!40000 ALTER TABLE `asignaturas` ENABLE KEYS */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
